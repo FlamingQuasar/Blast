2d4eab7f7401169db9a6c80b01cdac612cf499c7
FlamingQuasar <noomnau@gmail.com>
2024-09-24 15:20:20 +0300

    feat: adding core classes for base gameplay

---
+++ src/core/blastGame.js
@@ -0,0 +1,72 @@
+class BlastGame{
+    constructor({n, m, c, k=2}){
+       this.fieldHeight = n;
+       this.fieldWidth = m;
+       this.colorsCount = c;
+       this.minimalGroup = k<2?2:k;
+       this.hasPairs = false;
+       if(n && m){
+            this.createField();
+            this.checkField();
+       }
+    }
+
+    replaceItemsAfterFire(){
+        //type 1 : появляются сразу на местах
+
+        //type 2 : смещаются снизу вверх и генерятся сверху
+    }
+
+    createField(){
+        this.field=[];
+        for(let i=0; i<this.fieldHeight; i++){
+            let row = [];
+            this.field.push(row);
+            for(let j=0; j<this.fieldWidth;j++){
+                let item = new FieldItem(this.colorsCount);
+                row.push(item);
+                item.initNeighbours((j>0)?this.field[i][j-1]:null,
+                    (i>0)?this.field[i-1][j]:null,null,null);
+            }
+        }
+        console.log(this.field);
+    }
+
+    checkField(){
+        this.hasPairs = false;
+        outer:
+        for(let i=0; i<this.fieldHeight; i++){
+            for(let j=0; j<this.fieldWidth;j++){
+                this.hasPairs = this.field[i][j].hasSameNeighbour;
+                if(this.hasPairs) break outer;
+            }
+        }
+        console.log(this.hasPairs);
+    }
+
+    showField({onlyPairs = false}={}){
+        let fieldMatrix = "";
+        for(let i=0; i<this.fieldHeight; i++){
+            for(let j=0; j<this.fieldWidth;j++){
+                if(onlyPairs){
+                    fieldMatrix += `${this.field[i][j].hasSameNeighbour? this.field[i][j].color:"."} \t`;
+                }
+                else{
+                    fieldMatrix += `${this.field[i][j].color} \t`;
+                }
+            }
+            fieldMatrix += "\n";
+        }
+        console.log(fieldMatrix);
+    }
+
+    clickFieldItem(row, col){
+        if(this.field[row] === undefined 
+            || this.field[row][col] === undefined){
+            console.log("Такой клетки нет");
+            return;
+        }
+        this.field[row][col].fireItem();
+        this.showField();
+    }
+}
---
+++ src/core/fieldItem.js
@@ -0,0 +1,43 @@
+class FieldItem{
+    hasSameNeighbour = false;
+
+    constructor(colorsCount){
+        this.color = Math.floor(Math.random() * colorsCount);
+    }
+
+    checkNeighbourColorAndPush(neighbour){
+        if(neighbour?.color === this.color){
+            this.hasSameNeighbour = neighbour.hasSameNeighbour = true;
+        }
+    }
+
+    fireItem(){
+        if(this.hasSameNeighbour){
+            this.hasSameNeighbour = false;
+            if(this.left?.color == this.color && this.left?.hasSameNeighbour) this.left.fireItem();
+            if(this.top?.color == this.color && this.top?.hasSameNeighbour) this.top.fireItem();
+            if(this.right?.color == this.color && this.right?.hasSameNeighbour) this.right.fireItem();
+            if(this.bottom?.color == this.color && this.bottom?.hasSameNeighbour) this.bottom.fireItem();
+            this.color = "_";
+        }
+    }
+
+    initNeighbours(left, top, right, bottom){
+        this.left = left;
+        if(left != null) this.left.right = this;
+        this.top = top;
+        if(top != null) this.top.bottom = this;
+        this.right = right;
+        if(right != null) this.right.left = this;
+        this.bottom = bottom;
+        if(bottom != null) this.bottom.top = this;
+        this.checkNeighbours();
+    }
+
+    checkNeighbours(){
+        this.checkNeighbourColorAndPush(this.left);
+        this.checkNeighbourColorAndPush(this.top);
+        this.checkNeighbourColorAndPush(this.right);
+        this.checkNeighbourColorAndPush(this.bottom);
+    }
+}
